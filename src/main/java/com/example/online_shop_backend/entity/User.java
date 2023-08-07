88567d58e7e91d5c29b9cf27f65a2447026d9703
ehitimum <ehitimumchoudhury@gmail.com>
2023-08-07 12:11:11 +0600

    1.0

---
+++ src/main/java/com/example/online_shop_backend/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.online_shop_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_id_sequence",
+            sequenceName = "user_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_id_sequence"
+    )
+    private long userId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+}
---
+++ src/test/java/com/example/online_shop_backend/OnlineShopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
